0554100c6599d718191ffac78acfa6b7b9ce0674
Stalino A J <93860427+Stalinosmj@users.noreply.github.com>
2023-06-30 16:50:02 +0530

    object leak solved

--- calc.java
+++ calc.java
@@ -3,30 +3,31 @@ class calc
 {
 	public static void main(String args[])
 	{
-	  Scanner myObj = new Scanner(System.in);
-	  System.out.println("Enter first operand : ");
-	  float num1 = myObj.nextFloat();
-	  System.out.println("Enter second operand : ");
-	  float num2 = myObj.nextFloat();
-	  System.out.println("Enter the operator from below list : \n+\n-\n*\n/\n");
-	  char op = myObj.next().charAt(0);
-	  switch(op)
-	  {
-	  	case '+':
-	  	  System.out.println(num1+" + "+num2+" "+"= "+(num1+num2));
-	  	  break;
-	  	case '-':
-	  	  System.out.println(num1+" - "+num2+" "+"= "+(num1-num2));
-	  	  break;
-	  	case '*':
-	  	  System.out.println(num1+" * "+num2+" "+"= "+(num1*num2));
-	  	  break;
-	  	case '/':
-	  	  System.out.println(num1+" / "+num2+" "+"= "+(num1/num2));
-	  	  break;
-	  	default:
-	  	  System.out.println("Invalid");
-	  	  break;
-	  }	  
+	  try (Scanner myObj = new Scanner(System.in)) {
+		System.out.println("Enter first operand : ");
+		  float num1 = myObj.nextFloat();
+		  System.out.println("Enter second operand : ");
+		  float num2 = myObj.nextFloat();
+		  System.out.println("Enter the operator from below list : \n+\n-\n*\n/\n");
+		  char op = myObj.next().charAt(0);
+		  switch(op)
+		  {
+		  	case '+':
+		  	  System.out.println(num1+" + "+num2+" "+"= "+(num1+num2));
+		  	  break;
+		  	case '-':
+		  	  System.out.println(num1+" - "+num2+" "+"= "+(num1-num2));
+		  	  break;
+		  	case '*':
+		  	  System.out.println(num1+" * "+num2+" "+"= "+(num1*num2));
+		  	  break;
+		  	case '/':
+		  	  System.out.println(num1+" / "+num2+" "+"= "+(num1/num2));
+		  	  break;
+		  	default:
+		  	  System.out.println("Invalid");
+		  	  break;
+		  }
+	}	  
 	 } 	   
 }
